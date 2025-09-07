7c04aec63c7d41cfd848f644a6d71137a34e2f5e
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-09-07 19:52:19 +0600

    created user and quotes collection, implemented post api's

--- index.js
+++ index.js
@@ -25,7 +25,46 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         //await client.connect();
+        const usersCollection = client.db("sunnah_db").collection("users");
+        const quotesCollection = client.db("sunnah_db").collection("quotes");
 
+        app.post('/users', async (req, res) => {
+            try {
+                const user = req.body;
+                const existing = await usersCollection.findOne({
+                    email: user.email
+                });
+
+                if (existing) {
+                    return res.status(200).json({
+                        message: 'User already exists'
+                    });
+                }
+
+                const result = await usersCollection.insertOne(user);
+                res.status(201).json({
+                    insertedId: result.insertedId
+                });
+            } catch (err) {
+                console.log('Error adding user:', err);
+                res.status(500).json({
+                    error: 'Failed to add user'
+                });
+            }
+        })
+
+        app.post("/quotes", async (req, res) => {
+            try {
+                const newQuote = req.body;
+                const result = await quotesCollection.insertOne(newQuote);
+                res.status(201).json(result);
+            } catch (error) {
+                res.status(500).json({
+                    message: "Failed to add quote",
+                    error
+                });
+            }
+        });
 
         // await client.db("admin").command({
         //     ping: 1
